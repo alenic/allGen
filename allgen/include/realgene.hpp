9b0be5d33b3c7db90cdf574621488d816b7e100b
Nicolosi Alessandro (DS&G) <alessandro.nicolosi@enel.com>
2020-08-12 00:13:07 +0200

    added realgene mutatioN

--- allgen/include/realgene.hpp
+++ allgen/include/realgene.hpp
@@ -1,19 +1,30 @@
 #ifndef REALGENE_H
 #define REALGENE_H
 
+#include <string>
 #include "gene.hpp"
+#include "stat.hpp"
 
 namespace allgen {
+
+typedef enum {REAL_MUTATION_UNIFORM=0, REAL_MUTATION_GAUSSIAN} RealMutationType;
 
     template <class RealType>
     class RealGene: Gene {
     public:
-        RealGene(RealType t_lower_value, RealType t_upper_value);
+        RealGene(RealType t_min_value, RealType t_max_value);
         void mutate(float t_intensity) override;
         void random() override;
+        void setMutation(const std::string &t_mutation);
     private:
         RealType m_value;
+        RealType m_min_value;
+        RealType m_max_value;
+        RealMutationType m_mutation;
     };
+
+    typedef RealGene<float> FloatGene;
+    typedef RealGene<double> DoubleGene;
 
 }
 
--- allgen/src/realgene.cpp
+++ allgen/src/realgene.cpp
@@ -1,15 +1,48 @@
 #include "realgene.hpp"
+#include <vector>
+#include <iostream>
 
 using namespace allgen;
+using namespace std;
 
 template<class RealType>
-RealGene<RealType>::RealGene(RealType t_lower_value, RealType t_upper_value)
+RealGene<RealType>::RealGene(RealType t_min_value, RealType t_max_value)
 {
-    m_value = (t_lower_value + t_upper_value)/2.0;
+    m_value = (t_min_value + t_max_value)/2.0;
+    m_min_value = t_min_value;
+    m_max_value = t_max_value;
+    m_mutation = "uniform";
 }
 
 template<class RealType>
 void RealGene<RealType>::mutate(float t_intensity)
 {
-    //intensity is
+    if(m_mutation == REAL_MUTATION_UNIFORM) {
+        RealType half_interval = 0.5*t_intensity*(m_max_value-m_min_value);
+        m_value = Stat<RealType>::uniformRand(m_value - half_interval, m_value + half_interval);
+        if(m_value < m_min_value) m_value = m_min_value;
+        if(m_value > m_max_value) m_value = m_max_value;
+    } else if(m_mutation == REAL_MUTATION_GAUSSIAN) {
+        // TODO
+        cerr << "Error mutation gaussian to implement!" << endl;
+    }
+
+}
+
+template<class RealType>
+void RealGene<RealType>::random()
+{
+    m_value = Stat<RealType>::uniformRand(m_min_value, m_max_value);
+}
+
+template<class RealType>
+void RealGene<RealType>::setMutation(const std::string &t_mutation)
+{
+    if(t_mutation == "uniform") {
+        m_mutation = REAL_MUTATION_UNIFORM;
+    } else if(t_mutation == "gaussian") {
+        m_mutation = REAL_MUTATION_GAUSSIAN;
+    } else {
+        cerr << "Error: Invalid mutation type: " << t_mutation << endl;
+    }
 }
